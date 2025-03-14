8458da2f0ac0894fde0b70eef7ac04cf15c5b45e
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-03-14 14:54:49 +0100

    temporarily commenting out code

--- src/App.js
+++ src/App.js
@@ -1,37 +1,35 @@
 import { useState } from "react";
 
-function useGeolocation() {}
-
 export default function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [position, setPosition] = useState({});
-  const [error, setError] = useState(null);
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [position, setPosition] = useState({});
+  // const [error, setError] = useState(null);
 
   const [countClicks, setCountClicks] = useState(0);
   function handleClick() {
     setCountClicks((count) => count + 1);
   }
-  const { lat, lng } = position;
+  // const { lat, lng } = position;
 
-  function getPosition() {
-    if (!navigator.geolocation)
-      return setError("Your browser does not support geolocation");
+  // function getPosition() {
+  //   if (!navigator.geolocation)
+  //     return setError("Your browser does not support geolocation");
 
-    setIsLoading(true);
-    navigator.geolocation.getCurrentPosition(
-      (pos) => {
-        setPosition({
-          lat: pos.coords.latitude,
-          lng: pos.coords.longitude,
-        });
-        setIsLoading(false);
-      },
-      (error) => {
-        setError(error.message);
-        setIsLoading(false);
-      }
-    );
-  }
+  //   setIsLoading(true);
+  //   navigator.geolocation.getCurrentPosition(
+  //     (pos) => {
+  //       setPosition({
+  //         lat: pos.coords.latitude,
+  //         lng: pos.coords.longitude,
+  //       });
+  //       setIsLoading(false);
+  //     },
+  //     (error) => {
+  //       setError(error.message);
+  //       setIsLoading(false);
+  //     }
+  //   );
+  // }
 
   return (
     <div>
